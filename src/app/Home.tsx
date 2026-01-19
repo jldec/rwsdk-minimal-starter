b0392f0219ee84f774aa4b5013403a8216a5db3b
Jürgen Leschner <jldec@users.noreply.github.com>
2026-01-18 21:47:21 -0600

    bump rwsdk, add preload and view transitions (#12)

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -3,8 +3,11 @@ import { Button } from './Button'
 
 export function Home() {
   return (
-    <Layout title="rwsdk-minimal-starter Home">
-      <a href="https://github.com/jldec/rwsdk-minimal-starter" className="block text-blue-500 underline">
+    <Layout>
+      <a
+        href="https://github.com/jldec/rwsdk-minimal-starter"
+        className="block text-blue-500 underline"
+      >
         GitHub
       </a>
       <Button />
